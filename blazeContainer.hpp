b4949d37f3ac67617c8e8e6b0ed41afab7453cce
anu-ShOcKwAvE <45039902+anu-ShOcKwAvE@users.noreply.github.com>
2018-11-27 11:46:57 +0000

    Update blazeContainer.hpp

--- blazeContainer.hpp
+++ blazeContainer.hpp
@@ -1,6 +1,6 @@
 /*
                  THE BLAZE C++ LIBRARY
-                    <blazeContainer.hpp>
+                    <blazeContainer.hp
                 ---Written by: Anu Panicker---
                 
 Firstly, I am taking the time to thank all those people who gave me solutions to many of the seemingly impossible 
@@ -15,14 +15,6 @@ SPDX-License-Identifier: MIT
 Copyright (c) 2018 Anu Panicker. 
 Email: anupanicker@rocketmail.com
 Insta: middle.finger.is.my.peace.sign
-
-Permission is hereby granted, free of charge, to any  person obtaining a copy of this software and associated  documentation files (the "Software"), to deal in the Software without restriction, including without  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to  permit persons to whom the software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the software.
-
-THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
-IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE AUTHORS  OR COPYRIGHT  HOLDERS BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
-LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 DISCLAIMER: When I wrote this code only me and god understood it, at the time you are reading this only god will be able to understand this, so if you find any mistakes you have to deal with it yourself.
 */
